cda45c87161ff3aba7fb3c7c09b41d378fb524c0
Aris Koumis <ariskoumis@gmail.com>
2017-11-26 21:57:48 -0800

    Added skeleton login ui

--- public/js/index.js
+++ public/js/index.js
@@ -1,32 +1,12 @@
-var angular = require('angular')
+(function() {
+	angular.module('app', []).controller("login_page", function($scope) {
+		$scope.login = function(user) {
+			console.log(user.name)
+			console.log(user.password)
+		}
 
-$(document).ready(() => {
-	login_page.init()
-});
-
-var login_page = {
-	settings: {
-		app: angular.module('myApp', [])
-	}, 
-
-	init: function() {
-		setDebug(true);
-		$(document).off("keyup").on("keyup", function (event) {
-			var enterPressed = pressingKey(KEY_ENTER, event);	// checks for enter key press
-			if (enterPressed) {
-				console.log("pressed ENTER");
-				//User info in post currently has placeholder values.
-				post("login", {
-					"name": "hello",
-					"password": "world"
-				}, function (data,status,xhr) {
-					console.log(data);
-					console.log(status);
-					console.log(xhr);
-				});
-			}
-		});
-	},
-}
-
-	
+		$scope.createAccount = function() {
+			console.log("createAccount")
+		}
+	})
+})()
