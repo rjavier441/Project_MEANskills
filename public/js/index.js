38b7c782335818a56112e2412517b7274d48a4e0
Aris Koumis <ariskoumis@gmail.com>
2017-11-29 21:25:28 -0800

    User account creation now has fields corresponding to RJ's draw.io scheme

--- public/js/index.js
+++ public/js/index.js
@@ -34,56 +34,67 @@
 		}
 
 		$scope.createAccount = async function(new_user) {
-			//Check if user with that name already exists
-			var user_exists = false
-			await $.ajax({
-				url:"/findDoc",
-				method:"POST",
-				data: {	
-					"collection": "users",
-	 				"search": {
-	 					"username": new_user.name
- 					}
-				},
-				success: function(response) {
-					//convert response from string -> array
-					response = JSON.parse(response)
-					if (response.length != 0) {
-						user_exists = true
-						alert("That username is already in use.")	
-					} 
-				},
-				error: function(err) {
-					console.log(err.responseText)
-				}
-			})
+			var missing_input = (!new_user.username || !new_user.password || !new_user.first_name || !new_user.last_name || !new_user.major || !new_user.sid || !new_user.grad)
 
-			//If user doesn't exist already, create user
-			if (!user_exists) {
-				$.ajax({
-					url:"/writeNewDoc",
-					method:"POST", 
-					data: {
-						collection: 'users',
-						data: {
-							username: new_user.name,
-							password: new_user.password,
-							skills: $scope.skills,
-							classes: $scope.classes
-						}
+			if (missing_input) { 
+				alert("Cannot create account. Please fill out all fields.")
+			} else {
+				//Check if user with that name already exists
+				var user_exists = false
+				await $.ajax({
+					url:"/findDoc",
+					method:"POST",
+					data: {	
+						"collection": "users",
+		 				"search": {
+		 					"username": new_user.username
+	 					}
 					},
 					success: function(response) {
-						console.log(`account created for ${new_user.name}.`)
-						$scope.login({
-							name: new_user.name, 
-							password: new_user.password
-						})
+						//convert response from string -> array
+						response = JSON.parse(response)
+						if (response.length != 0) {
+							user_exists = true
+							alert("That username is already in use.")	
+						} 
 					},
 					error: function(err) {
 						console.log(err.responseText)
 					}
 				})
-			}
+
+				//If user doesn't exist already, create user
+				if (!user_exists) {
+					$.ajax({
+						url:"/writeNewDoc",
+						method:"POST", 
+						data: {
+							collection: 'users',
+							data: {
+								username: new_user.username,
+								first_name: new_user.first_name,
+								last_name: new_user.last_name,
+								password: new_user.password,
+								major: new_user.major,
+								grad: new_user.grad,
+								skills: $scope.skills,
+								classes: $scope.classes
+							}
+
+						},
+						success: function(response) {
+							console.log(`account created for ${new_user.username}.`)
+							$scope.login({
+								name: new_user.username, 
+								password: new_user.password
+							})
+						},
+						error: function(err) {
+							console.log(err.responseText)
+						}
+					})
+				}
+			}			
 		}
 
 		//Add class to array 'classes' on change
