3814451b531a4821af6121507fe810e790e9ef28
Aris Koumis <ariskoumis@gmail.com>
2017-11-29 16:49:39 -0800

    User cannot make account with a username that is already in use.

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@
 		$scope.show_test = true;
 		$scope.classes = [];
 		$scope.skills = []
+
 		$scope.login = function(user) {
 			$.ajax({
 				url:"/login",
@@ -12,7 +13,6 @@
 					password: user.password
 				},
 				success: function(response) {
-					console.log(response)
 					console.log(`user ${user.name} succesfully logged in`)
 				},
 				error: function() {
@@ -21,10 +21,10 @@
 			})
 		}
 
-		$scope.createAccount = function(new_user) {
+		$scope.createAccount = async function(new_user) {
 			//Check if user with that name already exists
 			var user_exists = false
-			$.ajax({
+			await $.ajax({
 				url:"/findDoc",
 				method:"POST",
 				data: {	
@@ -34,17 +34,19 @@
  					}
 				},
 				success: function(response) {
+					//convert response from string -> array
+					response = JSON.parse(response)
 					if (response.length != 0) {
 						user_exists = true
+						alert("That username is already in use.")	
 					} 
-					else {
-						alert("That username is already in use.")
-					}
 				},
 				error: function(err) {
 					console.log(err.responseText)
 				}
 			})
+
+			console.log("3: " + user_exists)
 			//If user doesn't exist already, create user
 			if (!user_exists) {
 				$.ajax({
@@ -60,8 +62,7 @@
 						}
 					},
 					success: function(response) {
-						console.log(response)
-						console.log(`user ${user.name} succesfully logged in`)
+						console.log(`account created for ${new_user.name}.`)
 					},
 					error: function(err) {
 						console.log(err.responseText)
@@ -70,6 +71,7 @@
 			}
 		}
 
+		//Add class to array 'classes' on change
 		$('#classes_dropdown li a').on('click', function(){
 			var new_class = $(this).text()
 		    if ($scope.classes.indexOf(new_class) == -1) {
@@ -78,6 +80,7 @@
 		    $scope.$apply();
 		});
 
+		//Add skill to array 'skills' on change
 		$('#skills_dropdown li a').on('click', function(){
 			var new_skill = $(this).text()
 			if ($scope.skills.indexOf(new_skill) == -1) {
@@ -86,14 +89,17 @@
 		    $scope.$apply();
 		});
 
+		//Remove class from array 'classes' on change
+		$scope.removeClass = function() {
+			var class_index = $scope.classes.indexOf(this.x)
+			$scope.classes.splice(class_index, 1)
+		}
+
+		//Remove skill from array 'skills' on change
 		$scope.removeSkill = function() {
 			var skill_index = $scope.skills.indexOf(this.x)
 			$scope.skills.splice(skill_index, 1)
 		}
 
-		$scope.removeClass = function() {
-			var class_index = $scope.classes.indexOf(this.x)
-			$scope.classes.splice(class_index, 1)
-		}
 	})
 })()
