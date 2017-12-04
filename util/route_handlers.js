f7c54a1c10393a4042f2e9f6b0afd6618c138dbd
Aris Koumis <ariskoumis@gmail.com>
2017-12-04 12:06:53 -0800

    delintRequestBody() was missing a closing bracket, also commented out searchForAris() and testRuntime()

--- util/route_handlers.js
+++ util/route_handlers.js
@@ -88,14 +88,14 @@ mongo.connect("mongodb://localhost:27017/testdb", function (err, db) {
 		});
 
 		//test Runtime of Searching Database.
-		for (var i=1; i<5; i++) {
-			var user_size = Math.pow(10, i)
-			console.log(`Searching for ariskoumis with ${user_size} other people in DB.`)
-			for (var j=0; j<10; j++) {
-				testRuntime(i)
-			}
-			console.log('\n')
-		}
+		// for (var i=1; i<5; i++) {
+		// 	var user_size = Math.pow(10, i)
+		// 	console.log(`Searching for ariskoumis with ${user_size} other people in DB.`)
+		// 	for (var j=0; j<10; j++) {
+		// 		testRuntime(i)
+		// 	}
+		// 	console.log('\n')
+		// }
 		
 	}
 });
@@ -734,6 +734,7 @@ function delintRequestBody (body, callback) {
 	} else {
 		callback(newBody);
   }
+}
 
 /*
 	@function 	numerify
@@ -894,46 +895,46 @@ function hashString(unhashed_string) {
 	@returns 	nothing
 	@details 	This function tests the runtime of MongoDB's searching for multiple collection sizes.
 */
-function testRuntime(users_needed) {
-	//create array which initializes with only ariskoumis
-	var user_array = [{username: "ariskoumis"}]
+// function testRuntime(users_needed) {
+// 	//create array which initializes with only ariskoumis
+// 	var user_array = [{username: "ariskoumis"}]
 
-	//populate user_array 
-	var users_created = 0
-	while (users_created < users_needed) {
-		var temp_user = {
-			username: Math.random().toString(36).substr(2, 7)
-		}
-		user_array.push(temp_user)
-		users_created++
-	}
+// 	//populate user_array 
+// 	var users_created = 0
+// 	while (users_created < users_needed) {
+// 		var temp_user = {
+// 			username: Math.random().toString(36).substr(2, 7)
+// 		}
+// 		user_array.push(temp_user)
+// 		users_created++
+// 	}
 
-	// Check if database collection exists
-	database.collection("users", {strict: true}, function (error, result) {
-		if (error != null) {
-			console.log(error)
-		} else {
-			result.remove({})
-			// Else, no error occurred, and the database collection was found; use it to write to the database
-			result.insertMany(user_array).then(function (promiseResult) {
-				console.log("Insertion successful")
-			});
-		}
-	});
+// 	// Check if database collection exists
+// 	database.collection("users", {strict: true}, function (error, result) {
+// 		if (error != null) {
+// 			console.log(error)
+// 		} else {
+// 			result.remove({})
+// 			// Else, no error occurred, and the database collection was found; use it to write to the database
+// 			result.insertMany(user_array).then(function (promiseResult) {
+// 				console.log("Insertion successful")
+// 			});
+// 		}
+// 	});
 
-	//Find User Aris Koumis
-	console.time('searchForAris')
-	searchForAris()
-	console.timeEnd('searchForAris')
-}
+// 	//Find User Aris Koumis
+// 	console.time('searchForAris')
+// 	searchForAris()
+// 	console.timeEnd('searchForAris')
+// }
 
-searchForAris = async function() {
-	await findDocs("users", {username: "ariskoumis"}, function(error, list) {
-		if (error != null) {
-			console.log(error)
-		}
-	})
-}
+// searchForAris = async function() {
+// 	await findDocs("users", {username: "ariskoumis"}, function(error, list) {
+// 		if (error != null) {
+// 			console.log(error)
+// 		}
+// 	})
+// }
 // END Utility Methods
 
 module.exports = handle_map;
